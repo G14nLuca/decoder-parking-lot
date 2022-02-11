cfd3a33208955d0e9de097e2dc0743580154324b
Gian Luca Oliveira <gloliveira539@gmail.com>
2022-02-11 19:52:45 -0300

    Initial setup

---
+++ src/main/java/edu/gianluca/parkinglotcontrol/ParkingLotControlApplication.java
@@ -0,0 +1,13 @@
+package edu.gianluca.parkinglotcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingLotControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/gianluca/parkinglotcontrol/ParkingLotControlApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.gianluca.parkinglotcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
